b30a3c1ac3987a44198ae88d72bda3978af9a62f
Tri Dat Quach <quachtridat@yahoo.com.vn>
2016-10-22 13:47:24 +0700

    Delete Array.cs
    Unused class.

--- Snake game/Array.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Snake_game {
-    class Array {
-        public static void Populate<T>(T[] arr, T value) {
-            for (int i = 0; i < arr.Length; ++i) arr[i] = value;
-        }
-    }
-}
